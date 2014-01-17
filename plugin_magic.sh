4d3d47288267d31d04a94efb0261678a710e8f0e
Miles Edland <miles.edlandm@gmail.com>
2014-01-17 11:09:50 -0800

    Add plugin-specific settings using vizardry's magic

---
+++ plugin_magic.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VIM_BUNDLE_DIR=$HOME/.vim/bundle
+VIZARDRY_DIR=$VIM_BUNDLE_DIR/vizardry
+MAGIC_DIR=$VIZARDRY_DIR/plugin/magic
+VIM_SRC_DIR=$HOME/vim/
+
+if [ -d VIM_SRC_DIR ];then
+    echo "Sorry, $VIM_SRC_DIR doesn't seem to exist so your files can't be found"
+    kill -SIGINT $$
+fi
+
+if [ ! -d $VIZARDRY_DIR ];then
+    cd $VIM_BUNDLE_DIR
+    git clone https://github.com/ardagnir/vizardry
+    cd - > /dev/null
+fi
+
+if [ ! -d $MAGIC_DIR ];then
+    echo "Creating $MAGIC_DIR"
+    mkdir $MAGIC_DIR
+fi
+
+for f in `ls -1 $VIM_SRC_DIR/plugin_magic`;do
+    if [ ! -e $VIM_SRC_DIR/plugin_magic/$f ];then
+        echo "Linking: $f"
+        ln -s $VIM_SRC_DIR/plugin_magic/$f $MAGIC_DIR
+    fi
+done
+
+echo "Done linking magic files."
---
+++ plugin_magic/magic_emmet-vim.vim
@@ -0,0 +1,5 @@
+" Enable Emmet in all modes
+let g:user_emmet_mode='a'
+
+" Set Emmet Leader Key -- default is: <C-y>
+let g:user_emmet_leader_key='<c-e>'
