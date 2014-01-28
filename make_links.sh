528edaa631905ab446bc77700ad47ae54a93abc5
Miles Edland <edlandm@wwu.edu>
2014-01-28 10:40:13 -0800

    Fix and improve make_links.sh

--- make_links.sh
+++ make_links.sh
@@ -7,30 +7,34 @@ ZSH_LOCAL=$HOME/.zsh
 ZSH_REMOTE=$DIR/zsh
 VIM_REMOTE=#DIR/vim
 
-if [ ! -e $HOME/.bash_profile ];then
+if [ ! -e `readlink $HOME/.bash_profile` ];then
     ln -s $DIR/.bash_profile $HOME/.bash_profile
 fi
 
-if [ ! -e $HOME/.vimrc ];then
+if [ ! -e `readlink $HOME/.vimrc` ];then
     ln -s $VIM_REMOTE/.vimrc $HOME/.vimrc
 fi
 
-if [! -e $HOME/.tmux.conf ];then
+if [ ! -e `readlink $HOME/.tmux.conf` ];then
     ln -s $DIR/.tmux.conf $HOME/.tmux.conf
 fi
 
-if [! -e $HOME/.tmux.reset.conf ];then
+if [ ! -e `readlink $HOME/.tmux.reset.conf` ];then
     ln -s $DIR/.tmux.reset.conf $HOME/.tmux.reset.conf
 fi
 
-if [! -e $HOME/.zshrc];then
+if [ ! -e `readlink $HOME/.zshrc` ];then
     ln -s $ZSH_REMOTE/.zshrc
 fi
 
-if [! -d $ZSH_LOCAL ];then
+if [ ! -d $ZSH_LOCAL ];then
     mkdir $ZSH_LOCAL
 fi
 
-if [! -e $ZSH_LOCAL/.alias* ];then
-    ln -s $ZSH_REMOTE/.alias* $ZSH_LOCAL
+if [ ! -e `readlink $ZSH_LOCAL/.alias.zsh` ];then
+    ln -s $ZSH_REMOTE/.alias.zsh $ZSH_LOCAL
+fi
+
+if [ ! -e `readlink $ZSH_LOCAL/.alias.local.zsh` ];then
+    ln -s $ZSH_REMOTE/.alias.local.zsh $ZSH_LOCAL
 fi
