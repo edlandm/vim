65324fe993e4ba5b4711d197b3008cfa1353507f
Miles Edland <miles.edlandm@gmail.com>
2014-07-28 00:30:53 -0700

    files

--- zsh/alias.laptop.zsh
+++
@@ -1,7 +0,0 @@
-# LOCAL ZSH Aliases: shutupmiles -- Miles Edland ============================================
-#===============================================================================
-alias images='mv ~/Downloads/*.(jpg|jpeg|png|pdf) ~/Downloads/images'
-alias gifs='mv ~/Downloads/*.gif ~/Downloads/gifs'
-
-# todo.txt aliases
-alias -g t='todo.sh'
